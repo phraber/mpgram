840bcd03523af023c7f2ed195d84e224753b4ece
Peter Hraber <peter@hraber.com>
2016-03-08 13:10:25 -0700

    Initial commit.

--- server.R
+++ server.R
@@ -1,30 +1,179 @@
 library(shiny)
-require(pixgramr)
+require(pixelgram)
 
 shinyServer(function(input, output, session) {
 
-    pg <- pixgramr::pixgram()
-    # disable the checkbox if any conditions are true:
+    pg <- pixelgram::pixelgram()
+
+    updateAln <- reactive({
+
+        aln.formats <- c("fasta", "clustal", "phylip", "msf", "mase")
+
+        validate(
+	    need(
+(input$alnFormat==1 & grepl("fas|fst", input$alnFile$name, ignore.case=T)) | 
+(input$alnFormat==2 & grepl("cl", input$alnFile$name, ignore.case=T)) | 
+(input$alnFormat==3 & grepl("phy", input$alnFile$name, ignore.case=T)) | 
+(input$alnFormat==4 & grepl("msf", input$alnFile$name, ignore.case=T)) | 
+(input$alnFormat==5 & grepl("mas", input$alnFile$name, ignore.case=T)),
+        paste0("Sorry but your alignment file name must contain '", 
+            aln.formats[as.numeric(input$alnFormat)], 
+#            input$alnFormat, 
+	    "' to ensure it is formatted as you intend."))
+    )
+
+        pixelgram::set.aln.file(pg, 
+	    input$alnFile$datapath, 
+	    input$alnType,
+            aln.formats[as.numeric(input$alnFormat)], 
+	    input$pngs2o)
+    })
+
+    updateTre <- reactive({
+        validate(
+            need(input$alnFile$datapath, 
+                 "Please specify an alignment file."),
+            need(c(input$treFile$datapath, 
+                   (input$createTree & input$alnType=="codon")),
+                 "Please specify a tree file or 'Build NJ tree from codon alignment'."),
+            need(is.numeric(input$pointSize), 
+                 "Please specify a numeric point size."),
+            need(input$pointSize > 0, 
+                 "Please specify a positive point size.")
+	)
+
+#        if (!is.null(input$treFile$datapath))
+            pixelgram::set.tre.file(updateAln(), input$treFile$datapath)
+#        else return (pg)
+    })
 
     output$myPixgramPlot <- renderPlot({
 
-        if (!is.null(input$treFile$datapath) | input$alnType == 'aa')
+        # unset the checkbox if any conditions are true:
+        if (input$createTree & 
+            (!is.null(input$treFile$datapath) | input$alnType == 'aa'))
             updateCheckboxInput(session, input$createTree, value=F)
 
-        if (is.null(pg$aas) & is.null(pg$nts) & !is.null(input$alnFile$datapath))
-            pg <- pixgramr::set.aln.file(pg, input$alnFile$datapath, input$alnType)
+#    if (input$tippch)
+#        tippch.parser <- pixelgram::create.timepoint.parser(input$tipcol.delimType, input$tipcol.fieldNumb)
 
-        if (is.null(pg$tre) & !is.null(input$treFile$datapath))
-            pg <- pixgramr::set.tre.file(pg, input$treFile$datapath)
+#    if (input$tipcol)
+#        tippch.parser <- pixelgram::create.timepoint.parser(input$tippch.delimType, input$tippch.fieldNumb)
 
-        if ((!is.null(pg$aas) | !is.null(pg$nts)) & 
-            (!is.null(pg$tre) | input$createTree))
+#        pg <- updateAln()
+        pg <- updateTre()
+
+        par(mar=c(0,0,0,0), oma=c(0,0,0,0), cex=input$pointSize/12)
+
+        plot(pg, #updateAln(updateTre()), 
+            xform_type = input$xformType,
+            show_tree = !input$hideTree,
+            xform_master = input$xformMaster,
+            show_tip_label = input$labelTips,
+            raster_width = input$pixelWidth/100)
+    })
+
+    output$downloadFile <- downloadHandler(
+
+        filename = function() {
+            paste0("pixelgram-", gsub(" ", "_", Sys.Date()), "-",
+                   input$outfileLayout, ".", input$outfileFormat) },
+
+        content = function(file) {
+
+            owd <- setwd(tempdir())
+            on.exit(setwd(owd))
+            tmpfile <- tempfile()#fileext=input$outfileFormat)
+            cat ( "\n", tmpfile, "\n", "i am", system("whoami"), "\n")
+
+            if (input$outfileFormat == "png") {
+
+                my.width = 8
+		my.height = 8
+
+		if (input$outfileLayout == "landscape")
+		    my.width=10.5
+	        else if (input$outfileLayout == "portrait")
+		    my.height=10.5
+
+                png(file=tmpfile, width=my.width*72, height=my.height*72, 
+		    pointsize=input$pointSize)
+
+            } else if (input$outfileFormat == "svg") {
+
+                my.width = 8
+                my.height = 8
+
+                if (input$outfileLayout == "landscape")
+		    my.width=10.5
+	        else if (input$outfileLayout == "portrait")
+		    my.height=10.5
+
+                svg(file=tmpfile, width=my.width, height=my.height, onefile=T,
+		    pointsize=input$pointSize)
+
+            } else if (input$outfileFormat == "eps") {
+
+                switch(input$outfileLayout,
+                    landscape = ps.options("paper"="us", "horizontal"=T,
+                        "title"=tmpfile, "height"=8.5, "width"=11),
+                    portrait = ps.options("paper"="us", "horizontal"=F,
+                        "title"=tmpfile, "height"=11, "width"=8.5),
+                    square = ps.options("paper"="special", 
+                        "title"=tmpfile, "height"=8, "width"=8))
+
+#		contentType = "application/postscript"
+
+                postscript(file=tmpfile)
+
+            } else if (input$outfileFormat == "pdf") {
+
+                switch(input$outfileLayout,
+                    landscape = pdf.options("compress"=F, "paper"="USr", 
+                        "title"=tmpfile, "height"=8.5, "width"=11, 
+			"useDingbats"=F, "pointsize"=input$pointSize),
+                    portrait = pdf.options("compress"=F, "paper"="US", 
+                        "title"=tmpfile, "height"=11, "width"=8.5, 
+			"useDingbats"=F, "pointsize"=input$pointSize),
+                    square = pdf.options("compress"=F, "paper"="special", 
+                        "title"=tmpfile, "height"=8, "width"=8, 
+			"useDingbats"=F, "pointsize"=input$pointSize))
+
+#		contentType = "application/pdf"
+		pdf(file=tmpfile)
+
+            } else { stop("ERROR: Unrecognized output file format") }
 
             par(mar=c(0,0,0,0), oma=c(0,0,0,0))
 
-            pg <- plot(pg, xform_type=input$xformType, 
-                       xform_master=input$xformMaster,
-                       show_tip_label=input$labelTips,
-                       raster_width=input$pixelWidth/100)
-    })
-})
+            plot(updateTre(),
+		xform_type = input$xformType, 
+		show_tree = !input$hideTree, 
+		xform_master = input$xformMaster,
+		show_tip_label = input$labelTips, 
+		raster_width = input$pixelWidth/100)
+
+            dev.off()
+
+        file.rename(tmpfile, file)
+    } #,
+
+# does this add filename suffixes?
+#    contentType = ifelse(input$outfileFormat == "pdf", 
+#        "application/pdf", 
+#	ifelse(input$outfileFormat == "eps", 
+#            "application/postscript",
+#            ifelse(input$outfileFormat == "png",
+#		"image/png",
+#		"image/svg+xml")))
+
+#    if (input$outfileFormat == "pdf") {
+#        contentType = "application/pdf"
+#    } else if (input$outfileFormat == "eps") {
+#        contentType = "application/postscript" }
+#    else if (input$outfileFormat == "png") {
+#        contentType = "image/png" }
+#    else if (input$outfileFormat == "svg") {
+#        contentType = "image/svg+xml"
+#    }
+)})
--- ui.R
+++ ui.R
@@ -1,47 +1,131 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-aln.types <- c('aa', 'nt', 'codon') # for friendly radio buttons
+
+aln.type <- c('aa', 'nt', 'codon') # for friendly radio buttons
+names(aln.type) <- c("Amino acids", "Nucleotides", "Codons")
+
+aln.formats <- c(1:5)
+names(aln.formats) <- c("fasta", "clustal", "phylip", "msf", "mase")
 
 xform.types <- c(0:3)
-
 names(xform.types) <- c('None', 
                         'Changes from master sequence', 
                         'Mutations from master sequence', 
                         'Charge changes from master sequence')
 
-names(aln.types) <- c("Amino acids", "Nucleotides", "Codons")
+outfile.layouts <- c("landscape", "portrait", "square")
+outfile.types <- c("pdf", "eps", "png") #"svg"
 
-shinyUI(fluidPage(
+shinyUI(
 
-  # Application title
-  titlePanel("Pixgram Maker"),
+    fluidPage(
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-        fileInput("alnFile", "Alignment File"),
-        radioButtons("alnType", "Alignment Type", aln.types),
-        fileInput("treFile", "Tree File"),
-        checkboxInput("createTree", "Create NJ tree from nucleotide sequences", value=F),
-        checkboxInput("labelTips", "Show Tree Tip Labels", value=T),
-        radioButtons("xformType", "Transformation Type", xform.types),
-        checkboxInput("xformMaster", "Transform Master Sequence", value=F),
-        sliderInput("pixelWidth",
-                    "Proportionate Width of Pixel View, %",
-                    min = 25,
-                    max = 425,
-                    value = 100),
-        submitButton("Update View")
-    ),
-    # Show results
-    mainPanel(
-      plotOutput("myPixgramPlot")
+        includeCSS("hiv-de.css"),
+
+        tags$head(
+            tags$link(rel="stylesheet", type="text/css", href="hiv-de.css")
+        ),
+
+        titlePanel("PixelGram: Pairs a Pixel plot of your alignment with a tree drawn as phyloGram, one row per sequence"),
+
+#   tabsetPanel(type="tabs", id="tabets", 
+#       tabPanel("Options",
+
+        fluidRow(
+            column(width=4, tags$div(class="paramtablehead", 'Alignment'),
+                tags$table(class="paramtable", 
+                    tags$tr(
+                        tags$td(
+
+                            fileInput("alnFile", "Alignment File"),
+
+                            radioButtons("alnType", "Sequence Type", aln.type),
+
+                            radioButtons("alnFormat", "Alignment Format", 
+                                aln.formats, inline=T),
+
+                            conditionalPanel("input.alnType != 'nt'",
+                                checkboxInput("pngs2o", 
+                                    "Map PNG asparagines to 'O' ", 
+                                    value=T)
+                            )
+                        )
+                    )
+                )
+            ), # end of column
+
+            column(width=4, tags$div(class="paramtablehead", 'Phylogeny'),
+                tags$table(class="paramtable", 
+                    tags$tr(
+                        tags$td(
+
+                            fileInput("treFile", "Newick-Formatted Tree File"),
+
+                            conditionalPanel("input.alnType=='codon'",
+                                checkboxInput("createTree", 
+                                    "Build NJ tree from codon alignment", 
+                                    value=F)
+                            ),
+
+                            checkboxInput("hideTree", "Hide Tree", value=F),
+
+                            conditionalPanel("input.hideTree==false",
+                                checkboxInput("labelTips", 
+                                    "Show Tree Tip Labels", value=T)),
+
+                            conditionalPanel("input.labelTips==true && input.hideTree==false",
+				numericInput("pointSize",
+                                        "Point Size",
+                                        min = 0,
+                                        max = 48,
+                                        value = 12, width="25%")),
+
+                            sliderInput("pixelWidth",
+                                        "Proportionate Width of Pixel View, %",
+                                        min = 10,
+                                        max = 400,
+                                        value = 100)
+                        ) # </td>
+                    ) # </tr>
+                ) # end of table
+            ), # end of column
+
+            column(width=4,
+
+                tags$div(class="paramtablehead", 'Output'),
+                    tags$table(class="paramtable", 
+                        tags$tr(
+                            tags$td(
+
+                                radioButtons("xformType", 
+                                    "Transformation Type", xform.types),
+
+                                conditionalPanel("input.xformType > 0",
+                                    checkboxInput("xformMaster", 
+                                        "Transform Master Sequence", value=F)
+                                ),
+
+                                radioButtons("outfileLayout", 
+                                    "Layout in Download File", 
+                                    outfile.layouts, inline=T),
+
+                                radioButtons("outfileFormat", 
+                                    "Download File Format", 
+                                    outfile.types, inline=T),
+
+                                downloadButton('downloadFile', 'Download')
+                        ) #</td>
+                    ) # </tr>
+                ) # end of table
+            ) # end of column
+        ), # end of row
+
+#tabPanel("Advanced",
+#),
+        fluidRow( 
+            column(width=12, # hr(),
+                tags$div(class="paramtablehead", 'Result'),
+                plotOutput("myPixgramPlot", width="100%", height=800)
+            )
+        )
     )
-  )
-))
+)
